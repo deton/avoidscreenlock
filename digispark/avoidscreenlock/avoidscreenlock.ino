cf1eb7e71fd3049067d020c54575f22559621ed4
KIHARA Hideto <deton@m1.interq.or.jp>
2015-12-27 17:38:56 +0900

    Digispark版でスクリーンロックが効かなかったのを修整

--- digispark/avoidscreenlock/avoidscreenlock.ino
+++ digispark/avoidscreenlock/avoidscreenlock.ino
@@ -1,9 +1,17 @@
-#include <DigiMouse.h>
+// DigiMouse.hだと、常に入力ありとみなされて(?)スクリーンセーバが動かなくなる。
+// DigiMouse.hの186-191行(time_since_last_report関係)と、140-142行
+// (last_send_reportのclear関係)コメントアウトしたDigiMouseQuiet.hを使用
+#include "DigiMouseQuiet.h"
 
 const int SENSORPIN = 2;
 const int LEDPIN = 1; // LED on Digispark Model A (rev2, rev4)
+#define LED_ONACTIVE 1
+#define LED_ONAWAY   2
+#define LEDON LED_ONAWAY
+#if (LEDON == LED_ONACTIVE)
 // 最後にセンサの反応がなくなってからこの時間が過ぎたらLED消灯
 const uint32_t LEDTIMEOUT = 15000; // [ms]
+#endif
 uint32_t now;
 uint32_t lastActive = 0;
 uint32_t lastMouse = 0;
@@ -22,6 +30,9 @@ static void mouseLoop()
     const uint32_t PCLOCKMS = 540000; // 9 [min]
     if (now - lastMouse > PCLOCKMS) {
         if (now - lastActive > PCLOCKMS) { // away presence
+#if (LEDON == LED_ONAWAY)
+            digitalWrite(LEDPIN, HIGH);
+#endif
             return; // allow screen saver
         }
         lastMouse = now;
@@ -34,14 +45,16 @@ void loop()
     now = millis();
     boolean isActive = (digitalRead(SENSORPIN) == HIGH);
     if (isActive) {
-        //digitalWrite(LEDPIN, HIGH);
         lastActive = now;
+#if (LEDON == LED_ONACTIVE)
+        digitalWrite(LEDPIN, HIGH);
     } else if (now - lastActive > LEDTIMEOUT) {
         digitalWrite(LEDPIN, LOW);
+#endif
     }
     mouseLoop();
-    // XXX: 入力ありとみなされて(?)スクリーンセーバが動かなくなる。
-    // が、(DigiMouse.update()も)呼ばなかったら、moveX()しても効かなくなる
-    //DigiMouse.delay(50);
-    delay(50);
+    DigiMouse.delay(50);
+#if (LEDON == LED_ONAWAY)
+    digitalWrite(LEDPIN, LOW);
+#endif
 }
